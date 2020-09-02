bcadf1235d39b3ffeb8e5da7b6d47cad457a9a2b
Srar <a1020938070@gmail.com>
2020-09-02 15:25:48 +0800

    增加 EQ符号

--- OperatorSymbol.go
+++ OperatorSymbol.go
@@ -147,6 +147,7 @@ func findOperatorPrecedenceForSymbol(symbol OperatorSymbol) operatorPrecedence {
 	Also used during evaluation to determine exactly which comparator is being used.
 */
 var comparatorSymbols = map[string]OperatorSymbol{
+	"=":  EQ,
 	"==": EQ,
 	"!=": NEQ,
 	">":  GT,
